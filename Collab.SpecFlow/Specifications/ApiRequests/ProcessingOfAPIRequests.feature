19584801f67d6f8eb8f94e574654a889dffdf72c
TNDStudios <info@thenakeddeveloper.com>
2019-01-24 23:51:05 +0000

    First stage check in, writing specflow scenarios in the feature file

---
+++ Collab.SpecFlow/Specifications/ApiRequests/ProcessingOfAPIRequests.feature
@@ -0,0 +1,18 @@
+﻿Feature: ProcessingOfAPIRequests
+	In order to process incoming API requests
+	As an object processor
+	I want to be able to route the object requests to the correct service bus
+
+@PersonRecordSentToApi
+Scenario: Incoming Request To Save A Person Record
+	Given a Person record has been transmitted to the Api
+	And the processor passes the basic validation of that record
+	When the request is processed by the routing logic
+	Then the Person record is placed on the Person service bus
+
+@AssetRecordSentToApi
+Scenario: Incoming Request To Save An Asset Record
+	Given a Asset record has been transmitted to the Api
+	And the processor passes the basic validation of that record
+	When the request is processed by the routing logic
+	Then the Assert record is placed on the Asset service bus
---
+++ Collab.SpecFlow/Specifications/ApiRequests/Steps/ProcessingOfAPIRequestsSteps.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TNDStudios.Tutorials.SpecFlowCollab.SpecFlow.Specifications.ApiRequests
+{
+    [Binding]
+    [Scope(Feature= "ProcessingOfAPIRequests")]
+    public class ProcessingOfAPIRequestsSteps
+    {
+        [Given(@"a Person record has been transmitted to the Api")]
+        public void GivenAPersonRecordHasBeenTransmittedToTheApi()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the processor passes the basic validation of that record")]
+        public void GivenTheProcessorPassesTheBasicValidationOfThatRecord()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"a Asset record has been transmitted to the Api")]
+        public void GivenAAssetRecordHasBeenTransmittedToTheApi()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the request is processed by the routing logic")]
+        public void WhenTheRequestIsProcessedByTheRoutingLogic()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the Person record is placed on the Person service bus")]
+        public void ThenThePersonRecordIsPlacedOnThePersonServiceBus()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the Assert record is placed on the Asset service bus")]
+        public void ThenTheAssertRecordIsPlacedOnTheAssetServiceBus()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
